8c1517f87b675d8c45981455735883c8246dd89f
danwardvs <danwardvs@adsgames.net>
2019-12-06 17:01:07 -0500

    stuff

--- VRmarblemaze/Assets/GamePlaneScript.cs
+++ VRmarblemaze/Assets/GamePlaneScript.cs
@@ -10,12 +10,12 @@ public class GamePlaneScript : MonoBehaviour
 
     }
 
-    public float smooth = 2.0F;
-    public float tiltAngle = 20.0F;
+    public float smooth = 0.3f;
+    public float tiltAngle = 5.0F;
     void Update()
     {
-        float tiltAroundZ = Input.GetAxis("Horizontal") * -tiltAngle;
-        float tiltAroundX = Input.GetAxis("Vertical") * -tiltAngle;
+        float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
+        float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
         Quaternion target = Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
         transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
 
